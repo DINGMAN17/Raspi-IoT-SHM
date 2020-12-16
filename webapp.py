47ecb916a99c8bb0131dcdd6d70b255ab15651c5
DING MAN <55436197+DINGMAN17@users.noreply.github.com>
2020-12-16 22:23:40 +0800

    update name of the menu

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '0000'
 app.config['DATABASE'] = database.SensorData()
 
-admin = Admin(app, name='Sensors', template_mode='bootstrap3', url='/')
+admin = Admin(app, name='Monitoring', template_mode='bootstrap3', url='/')
 admin.add_view(ModelView(database.Sensor))
 admin.add_view(ModelView(database.Scan))
 admin.add_view(ModelView(database.SensorReading))
